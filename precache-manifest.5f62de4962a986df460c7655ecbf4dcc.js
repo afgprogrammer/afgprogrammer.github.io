4ad934b11654b4eecaf12a44e744d0c93b3215ba
Mohammad Rahmani <rto1680@gmail.com>
2021-02-05 23:36:22 +0430

    Add new build

---
+++ precache-manifest.5f62de4962a986df460c7655ecbf4dcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b48b9bcd85786531602",
+    "url": "/css/app.ab074c9f.css"
+  },
+  {
+    "revision": "6f6bd8bf02f143dd1efde75c11ee7c28",
+    "url": "/images/afgprogrammer.jpeg"
+  },
+  {
+    "revision": "a08578309508ba097818199c55d24e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b48b9bcd85786531602",
+    "url": "/js/app.a9b8ea03.js"
+  },
+  {
+    "revision": "bd2473d79a70f48b2b13",
+    "url": "/js/chunk-vendors.89c9d5c1.js"
+  },
+  {
+    "revision": "136244e6101a266cc5ecc302fce4ce27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
