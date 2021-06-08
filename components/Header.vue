4b06198ca6daaa8d00bd26e04a8e7437bc48a2a8
Mohammad Rahmani <rto1680@gmail.com>
2021-06-08 21:57:50 +0430

    Add header

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header id="main-header" class="border-b border-gray-100 py-3">
+    <div class="flex justify-between items-center">
+      <Logo></Logo>
+      <nav class="nav text-gray-600">
+        <router-link to="/" title="afgprogrammer-home" class="m-4">Home</router-link>
+        <a href="/blog" title="afgprogrammer-blog">Blog</a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,7 @@
 <template>
-  <Nuxt />
+  <section id="app">
+    <Header class="mx-auto max-w-6xl"></Header>
+    <Nuxt />
+  </section>
 </template>
 
