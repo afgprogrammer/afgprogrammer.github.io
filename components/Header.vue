779e7e51c615359433008fb3d5a8e728c83e5d63
Mohammad Rahmani <rto1680@gmail.com>
2021-06-09 20:38:13 +0430

    Add active link style

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,9 @@
   <header id="main-header" class="border-b border-gray-100 py-3">
     <div class="flex justify-between items-center">
       <Logo></Logo>
-      <nav class="nav text-gray-600">
-        <router-link to="/" title="afgprogrammer-home" class="m-4">Home</router-link>
-        <a href="/blog" title="afgprogrammer-blog">Blog</a>
+      <nav class="text-gray-600 text-sm">
+        <router-link to="/" title="afgprogrammer-home" class="m-4" :class="{'font-medium text-gray-900': !isRouteActive('blog')}">Home</router-link>
+        <a href="/blog" title="afgprogrammer-blog" :class="{'font-medium text-gray-900': isRouteActive('blog')}">Blog</a>
       </nav>
     </div>
   </header>
@@ -12,6 +12,10 @@
 
 <script>
 export default {
-
+  methods: {
+    isRouteActive(name) {
+      return this.$route.path.includes(name)
+    }
+  }
 }
 </script>
