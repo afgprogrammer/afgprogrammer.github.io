87c28750b41e22bfcd499431b6e7b2b3a3193f44
Mohammad Rahmani <rto1680@gmail.com>
2021-06-10 23:09:53 +0430

    Make header responsive

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header id="main-header" class="border-b border-gray-100 py-3">
+  <header id="main-header" class="border-b border-gray-100 py-3 px-6 xl:px-0">
     <div class="flex justify-between items-center">
       <Logo></Logo>
       <nav class="text-gray-600 text-sm">
