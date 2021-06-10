bca4e264d095cc944fa02e574525ded7d50b8d81
Mohammad Rahmani <rto1680@gmail.com>
2021-06-10 23:11:03 +0430

    Add seo tags and make responsive

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,21 +1,19 @@
 <template>
-  <article class="mx-auto max-w-6xl">
+  <article class="mx-auto max-w-6xl px-6 lg:px-0">
     <div class="flex py-6">
       <breadcrumb :links="breadcrumbLinks()"></breadcrumb>
     </div>
     <div class="h-4"></div>
-    <div class="grid grid-cols-2 gap-8">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
       <div>
         <img :src="article.image" :alt="article.title">
       </div>
       <div class="grid">
         <div>
           <h1 class="text-4xl font-bold tracking-wide">{{ article.title }}</h1>
-          <p class="mt-8 text-lg text-gray-700 font-light">
-            {{ article.description }}
-          </p>
+          <div class="mt-8 text-lg text-gray-700 font-light" v-html="article.description"></div>
         </div>
-        <div class="self-end">
+        <div class="self-end mt-9 lg:mt-0">
           <span class="text-xs text-gray-600">{{ formatDate(article.date) }}</span>
           <div>
             <span v-for="(tag, index) in article.tags" :key="index" class="mr-5 text-blue-900">{{ tag }}</span>
@@ -23,7 +21,15 @@
         </div>
       </div>
     </div>
-    <nuxt-content :document="article" />
+    <div class="h-4"></div>
+    <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-2490274469917078"
+      data-ad-slot="3173124995"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+    <div class="h-4"></div>
+    <!-- <nuxt-content :document="article" class="py-9"/> -->
   </article>
 </template>
 <script>
@@ -31,8 +37,69 @@ export default {
   async asyncData({ $content, params }) {
     const article = await $content('articles', params.slug).fetch()
 
-    console.log(article, params)
     return { article }
+  },
+
+  head () {
+    return {
+      title: this.article.title,
+      meta: [
+        { 
+          name: 'author', 
+          content: 'Mohammad Rahmani - afgprogrammer' 
+        },
+        {
+          name: 'description',
+          content: this.article.description
+        },
+        {
+          key: 'og:title',
+          property: 'og:title',
+          content: `${this.article.title}`,
+        },
+        {
+          key: 'og:image',
+          property: 'og:image',
+          content: `https://afgprogrammer.com${this.article.image}`,
+        },
+        {
+          key: 'og:description',
+          property: 'og:description',
+          content: this.article.description,
+        },
+        {
+          key: 'og:url',
+          property: 'og:url',
+          content: `https://afgprogrammer.com${this.article.path}`,
+        },
+        {
+          key: 'twitter:title',
+          name: 'twitter:title',
+          content: `${this.article.title}`,
+        },
+        
+        {
+          key: 'twitter:description',
+          name: 'twitter:description',
+          content: this.article.description,
+        },
+        {
+          key: 'twitter:creator',
+          name: 'twitter:creator',
+          content: '@afgprogrammer',
+        },
+        {
+          key: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          key: 'twitter:image',
+          name: 'twitter:image',
+          content: `https://afgprogrammer.com${this.article.image}`,
+        }
+      ]
+    }
   },
 
   methods: {
