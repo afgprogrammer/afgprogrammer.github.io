bc286b05dbcf329dc5ca539d518f99203d34b48b
Mohammad Rahmani <rto1680@gmail.com>
2021-06-11 14:28:39 +0430

    Deploy

--- dist/_nuxt/static/1623403823/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623403823",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6b56fa8f"}}}}(null,"\u002F"));
