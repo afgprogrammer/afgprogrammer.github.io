b15e4fea55bb721d125858c0a2af4f29a6be3491
Mohammad Rahmani <rto1680@gmail.com>
2021-06-11 18:19:06 +0430

    Deploy

--- dist/_nuxt/static/1623407392/blog/variables-in-php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623407392",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fvariables-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a6ba3086"}}}}(null));
