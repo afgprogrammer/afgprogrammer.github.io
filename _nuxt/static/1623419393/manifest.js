3ed5c0acce4a7360d730608716969a70ebae7ef8
Mohammad Rahmani <rto1680@gmail.com>
2021-06-11 18:21:13 +0430

    Deploy

--- _nuxt/static/1623419393/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fphp","\u002Fphp\u002Fswitch-case-in-php","\u002Fphp\u002Fconditional-statements-in-php","\u002Fphp\u002Fphp-case-sensitivity","\u002Fphp\u002Fcomments-in-php","\u002Fphp\u002Fnumber-functions-in-php","\u002Fphp\u002Fvariables-in-php","\u002Fphp\u002Fstring-functions-in-php","\u002Fphp\u002Fthe-php-echo-statement"]})
