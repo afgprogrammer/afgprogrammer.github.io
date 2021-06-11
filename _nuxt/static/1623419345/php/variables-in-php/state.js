aae77965af42133d2f06ce14b7add380276bdbef
Mohammad Rahmani <rto1680@gmail.com>
2021-06-11 18:19:55 +0430

    Deploy

--- _nuxt/static/1623419345/php/variables-in-php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623419345",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fvariables-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fc0e6cef"}}}}(null));
---
+++ _nuxt/static/1623419393/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623419393",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a9428c98"}}}}(null,"\u002F"));
