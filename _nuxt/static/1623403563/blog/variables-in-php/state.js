b9f7cae2df6908a777ed919b29e043d5e7cb3f93
Mohammad Rahmani <rto1680@gmail.com>
2021-06-11 14:01:05 +0430

    Add new build

--- _nuxt/static/1623403563/blog/variables-in-php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623403563",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fvariables-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ccbb52db"}}}}(null));
