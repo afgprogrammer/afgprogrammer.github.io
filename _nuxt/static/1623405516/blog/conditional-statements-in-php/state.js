3051a9f5d38023ef7fc5a47ac24f6499c97a3a81
Mohammad Rahmani <rto1680@gmail.com>
2021-06-11 14:28:39 +0430

    Deploy

---
+++ _nuxt/static/1623405516/blog/conditional-statements-in-php/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623405516",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fconditional-statements-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0673b072"}}}}(null));
