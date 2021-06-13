6425c5b071184bf260b7caf5ebc98f37c7a190ad
Mohammad Rahmani <rto1680@gmail.com>
2021-06-13 18:00:10 +0430

    Deploy

---
+++ _nuxt/static/1623591006/php/for-loop-in-php/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623591006",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Ffor-loop-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1ff0d4a9"}}}}(null));
