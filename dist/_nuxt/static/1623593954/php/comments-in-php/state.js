1f443dbde639278718a2815fce0d6fd5af539692
Mohammad Rahmani <rto1680@gmail.com>
2021-06-15 18:46:39 +0430

    Deploy

--- dist/_nuxt/static/1623593954/php/comments-in-php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623593954",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fcomments-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7aee57b3"}}}}(null));
