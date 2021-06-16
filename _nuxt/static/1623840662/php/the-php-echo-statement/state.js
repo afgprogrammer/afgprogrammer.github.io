a77afcc247f606841f32ca66522a6e7984da877a
Mohammad Rahmani <rto1680@gmail.com>
2021-06-16 15:21:05 +0430

    Deploy

---
+++ _nuxt/static/1623840662/php/the-php-echo-statement/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623840662",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fthe-php-echo-statement",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6ace05ea"}}}}(null));
