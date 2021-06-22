4b2a8845c6504ebb480dc391a5581bf8ab38acc0
Mohammad Rahmani <rto1680@gmail.com>
2021-06-22 12:17:14 +0430

    Deploy

--- dist/_nuxt/static/1624256794/php/switch-case-in-php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624256794",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fswitch-case-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f964e556"}}}}(null));
---
+++ dist/_nuxt/static/1624348033/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624348033",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"49faefa0"}}}}(null,"\u002F"));
