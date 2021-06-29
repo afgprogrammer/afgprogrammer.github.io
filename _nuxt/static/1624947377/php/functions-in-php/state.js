d55e2fae0f7a0e7a14fe1aa0a9adfe0e23087b4a
Mohammad Rahmani <rto1680@gmail.com>
2021-06-29 10:46:21 +0430

    Deploy

---
+++ _nuxt/static/1624947377/php/functions-in-php/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624947377",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Ffunctions-in-php",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b5dc8e3"}}}}(null));
