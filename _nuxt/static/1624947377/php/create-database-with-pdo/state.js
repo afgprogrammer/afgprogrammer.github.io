2c96a32fd32cf27bc8e7ebd1f541d60bee2cc589
Mohammad Rahmani <rto1680@gmail.com>
2021-06-30 09:18:28 +0430

    Deploy

--- _nuxt/static/1624947377/php/create-database-with-pdo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624947377",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fcreate-database-with-pdo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b5dc8e3"}}}}(null));
--- _nuxt/static/1624947377/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624947377",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b5dc8e3"}}}}(null,"\u002F"));
