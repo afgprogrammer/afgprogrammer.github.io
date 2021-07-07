fbff7606e872dcb144e2ab5792df079110334b96
Mohammad Rahmani <rto1680@gmail.com>
2021-07-07 19:34:52 +0430

    Deploy

---
+++ dist/_nuxt/static/1625670289/php/php-ucwords-function/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625670289",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fphp-ucwords-function",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b10a450e"}}}}(null));
---
+++ dist/_nuxt/static/1625670289/presentations/how-to-write-clean-code-in-laravel/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625670289",layout:"presentation",error:a,serverRendered:true,routePath:"\u002Fpresentations\u002Fhow-to-write-clean-code-in-laravel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b10a450e"}}}}(null));
