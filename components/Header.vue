042cea548600d86519cc5da8a1ce582fa6129ffd
Mohammad Rahmani <rto1680@gmail.com>
2021-07-21 22:33:45 +0430

    Add flutter link to navbar

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,8 @@
     <div class="flex justify-between items-center">
       <Logo></Logo>
       <nav class="text-gray-600 text-sm">
-        <a href="/php" title="afgprogrammer-blog" class="mx-4" :class="{'font-medium text-gray-900': isRouteActive('php')}">PHP</a>
+        <a href="/php" title="afgprogrammer-blog" :class="{'font-medium text-gray-900': isRouteActive('php')}">PHP</a>
+        <a href="/flutter" title="afgprogrammer-flutter" class="mx-4" :class="{'font-medium text-gray-900': isRouteActive('flutter')}">Flutter</a>
         <a href="/presentations" title="presentations - afgprogrammer" :class="{'font-medium text-gray-900': isRouteActive('presentation')}">Presentations</a>
       </nav>
     </div>
