0d0ee17283fa57a096852d909b994770f4fc99a1
Mohammad Rahmani <rto1680@gmail.com>
2021-07-26 00:10:48 +0430

    Deploy

---
+++ dist/_nuxt/static/1627242045/blog/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/blog", {data:[{posts:[{slug:"how-to-use-svg-icons-in-vue",title:"How to use SVG icons in Vue with vue-uicon",date:"2021-07-25T14:22:52.000Z",image:"\u002Fimages\u002Fblog\u002Fvue-uicon.png",summery:"vue-uicon is a Vue component that allows you to use SVG icons easily in your Vue application."}]}],fetch:{},mutations:void 0});
