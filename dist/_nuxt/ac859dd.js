ebb62e9d868b2e5a996dbc57a55a970572f447df
Mohammad Rahmani <rto1680@gmail.com>
2021-07-26 00:03:32 +0430

    Deploy

--- dist/_nuxt/ac859dd.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{279:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){return location.href="/php"}},r=e(14),component=Object(r.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
