cf7f3faac2f87efa68091c5cc29ff471b60347b5
Mohammad Rahmani <rto1680@gmail.com>
2021-07-26 00:02:11 +0430

    Add blog link to navbar

--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,7 @@
     <div class="flex justify-between items-center">
       <Logo></Logo>
       <nav class="text-gray-600 text-sm">
+        <a href="/blog" title="afgprogrammer-blog" class="mx-4" :class="{'font-medium text-gray-900': isRouteActive('blog')}">Blog</a>
         <a href="/php" title="afgprogrammer-blog" :class="{'font-medium text-gray-900': isRouteActive('php')}">PHP</a>
         <a href="/flutter" title="afgprogrammer-flutter" class="mx-4" :class="{'font-medium text-gray-900': isRouteActive('flutter')}">Flutter</a>
         <a href="/presentations" title="presentations - afgprogrammer" :class="{'font-medium text-gray-900': isRouteActive('presentation')}">Presentations</a>
