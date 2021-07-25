09fdff16ed1e043e8a7c6015159a7469b3c6842d
Mohammad Rahmani <rto1680@gmail.com>
2021-07-26 00:01:57 +0430

    Add blog section

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,135 @@
+<template>
+  <article class="mx-auto max-w-7xl px-6 xl:px-0">
+    <div class="flex py-6">
+      <breadcrumb :links="breadcrumbLinks()"></breadcrumb>
+    </div>
+    <div class="h-4"></div>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <div>
+        <img :src="blog.image" :alt="blog.title" class="rounded-t-lg">
+      </div>
+      <div class="grid">
+        <div>
+          <h1 class="text-4xl font-bold tracking-wide">{{ blog.title }}</h1>
+          <div class="mt-8 text-lg text-gray-700 font-light" v-html="blog.description"></div>
+        </div>
+        <div class="self-end grid grid-cols-2 mt-9 lg:mt-0">
+          <div>
+            <span class="text-xs text-gray-600">{{ formatDate(blog.date) }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-4"></div>
+    <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-2490274469917078"
+      data-ad-slot="9754475321"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+      <div class="h-4"></div>
+    <nuxt-content :document="blog" class="py-9"/>
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const blog = await $content('blog', params.slug).fetch()
+
+    return { blog }
+  },
+
+  head () {
+    return {
+      title: `${this.blog.title} - afgprogrammer`,
+      meta: [
+        { 
+          name: 'author', 
+          content: 'Mohammad Rahmani - afgprogrammer' 
+        },
+        {
+          name: 'description',
+          content: this.blog.summery
+        },
+        {
+          key: 'og:title',
+          property: 'og:title',
+          content: `${this.blog.title}`,
+        },
+        {
+          key: 'og:image',
+          property: 'og:image',
+          content: `https://afgprogrammer.com${this.blog.image}`,
+        },
+        {
+          key: 'og:description',
+          property: 'og:description',
+          content: this.blog.summery,
+        },
+        {
+          key: 'og:url',
+          property: 'og:url',
+          content: `https://afgprogrammer.com${this.blog.path}`,
+        },
+        {
+          key: 'twitter:title',
+          name: 'twitter:title',
+          content: `${this.blog.title}`,
+        },
+        
+        {
+          key: 'twitter:description',
+          name: 'twitter:description',
+          content: this.blog.summery,
+        },
+        {
+          key: 'twitter:creator',
+          name: 'twitter:creator',
+          content: '@afgprogrammer',
+        },
+        {
+          key: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          key: 'twitter:image',
+          name: 'twitter:image',
+          content: `https://afgprogrammer.com${this.blog.image}`,
+        }
+      ]
+    }
+  },
+
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    },
+
+    breadcrumbLinks() {
+      return [
+        {
+          title: 'Home',
+          to: '/'
+        },
+        {
+          title: 'Blog',
+          to: '/blog'
+        },
+        {
+          title: this.blog.title,
+          to: this.blog.slug
+        },
+      ]
+    }
+  }
+}
+</script>
+<style>
+.nuxt-content-highlight pre {
+  border-radius: 10px;
+  padding: 25px;
+  background-color: #212733;
+}
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,10 +1,105 @@
 <template>
-  
+  <section class="mx-auto max-w-7xl">
+    <div class="flex py-6 px-6 xl:px-0">
+      <breadcrumb :links="breadcrumbLinks()"></breadcrumb>
+    </div>
+    <div class="h-4"></div>
+    <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-2490274469917078"
+      data-ad-slot="9238225491"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+    <div class="h-4"></div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-6 lg:px-0">
+      <div v-for="(post, index) of posts" :key="index">
+        <router-link :to="{ name: 'blog-slug', params: { slug: post.slug } }">
+          <img :data-src="post.image" :alt="post.title" class="rounded-t-lg" v-lazy-load/>
+          <div class="mt-5">
+            <h2 class="text-xl font-medium text-gray-800">{{ post.title }}</h2>
+            <p class="mt-3 text-gray-600 text-sm">{{ post.summery.substring(0, 150) }} ...</p>
+          </div>
+        </router-link>
+      </div>
+    </div>
+    <div class="h-28"></div>
+  </section>
 </template>
 <script>
 export default {
-  mounted() {
-    return location.href = '/php'
+  async asyncData({ $content }) {
+    let posts = await $content('blog')
+      .only(['title', 'summery', 'image', 'slug', 'date',])
+      .sortBy('date', 'desc')
+      .fetch()
+
+    return {
+      posts
+    }
+  },
+
+  head: {
+    title: 'Afgprogrammer blog',
+    meta: [
+      { 
+        name: 'author', 
+        content: 'Mohammad Rahmani - afgprogrammer' 
+      },
+      {
+        name: 'description',
+        content: 'vue-uicon is a Vue component that allows you to use SVG icons easily in your Vue application.',
+      },
+      {
+        key: 'og:title',
+        property: 'og:title',
+        content: 'Afgprogrammer blog',
+      },
+      {
+        key: 'og:description',
+        property: 'og:description',
+        content: 'vue-uicon is a Vue component that allows you to use SVG icons easily in your Vue application.',
+      },
+      {
+        key: 'og:url',
+        property: 'og:url',
+        content: 'https://afgprogrammer.com/blog',
+      },
+      {
+        key: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Afgprogrammer blog',
+      },
+      {
+        key: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary',
+      },
+      {
+        key: 'twitter:creator',
+        name: 'twitter:creator',
+        content: '@afgprogrammer',
+      },
+      {
+        key: 'twitter:description',
+        name: 'twitter:description',
+        content: 'vue-uicon is a Vue component that allows you to use SVG icons easily in your Vue application.',
+      }
+    ]
+  },
+  
+  methods: {
+    breadcrumbLinks() {
+      return [
+        {
+          title: 'Home',
+          to: '/'
+        },
+        {
+          title: 'Blog',
+          to: '/blog'
+        }
+      ]
+    }
   }
 }
 </script>
