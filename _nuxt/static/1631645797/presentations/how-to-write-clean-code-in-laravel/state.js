adb2005e14630703ceb905a77bb92b1586d5a931
Mohammad Rahmani <rto1680@gmail.com>
2021-09-22 00:39:13 +0430

    Deploy

--- _nuxt/static/1631645797/presentations/how-to-write-clean-code-in-laravel/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631645797",layout:"presentation",error:a,serverRendered:true,routePath:"\u002Fpresentations\u002Fhow-to-write-clean-code-in-laravel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3e1eba9e"}}}}(null));
