b54d5379b340bf0fc6dffa719258ed07c45c2578
Mohammad Rahmani <rto1680@gmail.com>
2021-09-22 12:22:17 +0430

    Deploy

--- _nuxt/static/1632254950/php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632254950",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2590ea12"}}}}(null));
---
+++ _nuxt/static/1632297134/presentations/how-to-write-clean-code-in-laravel/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632297134",layout:"presentation",error:a,serverRendered:true,routePath:"\u002Fpresentations\u002Fhow-to-write-clean-code-in-laravel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7d1a920b"}}}}(null));
