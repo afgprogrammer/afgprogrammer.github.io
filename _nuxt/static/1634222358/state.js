7352dc51367e910afde8f2e153bbe9dd9f7a679a
Mohammad Rahmani <rto1680@gmail.com>
2021-10-15 17:57:16 +0430

    Deploy

--- _nuxt/static/1634222358/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634222358",layout:"default",error:a,serverRendered:true,routePath:b,config:{"google-adsense":{id:void 0,test:false},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e031f0fc"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1634304432/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634304432",layout:"default",error:a,serverRendered:true,routePath:b,config:{"google-adsense":{id:void 0,test:false},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bb3f4cd7"}}}}(null,"\u002F"));
