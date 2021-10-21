404d24ce7bd62740d7299cf35afcc6dc2bda37d8
Mohammad Rahmani <rto1680@gmail.com>
2021-10-21 20:29:39 +0430

    Deploy

---
+++ _nuxt/static/1634831976/php/insert-data-with-pdo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634831976",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Finsert-data-with-pdo",config:{"google-adsense":{id:void 0,test:false},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3ea6c38a"}}}}(null));
