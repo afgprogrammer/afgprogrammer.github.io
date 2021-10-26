be9ef2d96134686eaacff23f65e534366c0f998e
Mohammad Rahmani <rto1680@gmail.com>
2021-10-26 19:02:14 +0430

    Deploy

--- _nuxt/static/1635178996/php/select-data-with-pdo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635178996",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fselect-data-with-pdo",config:{"google-adsense":{id:void 0,test:false},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"879d030a"}}}}(null));
