fa6d711532ed995596d843dc07b2b9f9a342dca2
Mohammad Rahmani <rto1680@gmail.com>
2021-11-03 20:39:41 +0430

    Deploy

--- dist/_nuxt/static/1635955133/blog/how-to-use-svg-icons-in-vue/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635955133",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhow-to-use-svg-icons-in-vue",config:{"google-adsense":{id:void 0,test:false},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"78b6e368"}}}}(null));
