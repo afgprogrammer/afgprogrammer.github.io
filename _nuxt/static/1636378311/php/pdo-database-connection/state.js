799142179111a9361bb0ddada0d83486d34cf10c
Mohammad Rahmani <rto1680@gmail.com>
2021-11-08 18:01:55 +0430

    Deploy

---
+++ _nuxt/static/1636378311/php/pdo-database-connection/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636378311",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fpdo-database-connection",config:{"google-adsense":{id:void 0,test:false},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"77da694a"}}}}(null));
