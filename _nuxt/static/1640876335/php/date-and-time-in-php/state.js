b38bd98551956abd626951158b9a4b0205cc3598
Mohammad Rahmani <rto1680@gmail.com>
2021-12-30 19:32:12 +0430

    Deploy

--- _nuxt/static/1640876335/php/date-and-time-in-php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640876335",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fdate-and-time-in-php",config:{"google-adsense":{id:void 0,test:false},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dfb24280"}}}}(null));
