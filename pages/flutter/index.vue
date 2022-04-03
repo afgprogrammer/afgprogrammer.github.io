f6cca05141db31555f365b477077e2589a7ea23a
Mohammad Rahmani <rto1680@gmail.com>
2022-04-03 22:35:21 +0430

    Add search box

--- pages/flutter/index.vue
+++ pages/flutter/index.vue
@@ -6,9 +6,17 @@
       ad-format="auto"
       ad-label="flutter-ads"
     />
+    <div class="h-8"></div>
+    <div class="flex">
+      <div class="w-full border border-gray-100 rounded-xl p-6 ">
+        <p class="mb-4 text-lg font-medium">Search in 100+ Flutter Examples </p>
+        <div class="h-4"></div>
+        <input type="text" v-model="input" placeholder="search eg. day 22, GetX, Login" class="border border-gray-200 focus:border-blue-600 focus:shadow-md outline-none text-gray-600 text-md py-3 px-4 rounded-lg w-full bg-gray-100">
+      </div>
+    </div>
     <div class="h-4"></div>
-    <div class="grid grid-cols-1 md:grid-cols-2 pt-8 gap-10" v-if="days">
-      <div v-for="(day, index) in days" :key="index" class="bg-white rounded-lg border-2 border-gray-100">
+    <div class="grid grid-cols-1 md:grid-cols-2 pt-8 gap-10" v-if="days && days.length">
+      <div v-for="(day, index) in days" :key="index" class="bg-white rounded-xl border-2 border-gray-100">
         <div class="">
           <iframe class="rounded-t-xl" width="100%" height="400" :src="day.video.replace('youtu.be', 'youtube.com/embed')" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           <div class="p-4 pr-2">
@@ -19,6 +27,14 @@
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div v-else>
+      <div class="text-center mt-10">
+        <h1 class="text-xl font-bold tracking-wide">No Flutter Examples Found</h1>
+        <p class="text-gray-600 text-lg mt-4">
+          Please try another search
+        </p>
       </div>
     </div>
     <div class="h-40"></div>
@@ -32,6 +48,60 @@ export default {
     days = days[0].data.reverse()
 
     return { days }
+  },
+
+  data() {
+    return {
+      timeout: null,
+      searchKey: '',
+      days: [],
+      originalDays: [],
+    }
+  },
+
+  mounted() {
+    this.originalDays = this.days
+  },
+
+  computed: {
+    input: {
+      get() {
+        return this.searchKey
+      },
+      set(value) {
+        if (this.timeout) clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => {
+          this.search(value)
+        }, 500)
+      }
+    }
+  },
+
+  methods: {
+    search(value) {
+      this.searchKey = value
+
+      if (value.length > 0) {
+        this.days = this.originalDays.filter(day => {
+          return day.name.toLowerCase().includes(value.toLowerCase())
+        })
+      } else {
+        this.days = this.originalDays
+      }
+    },
+
+    breadcrumbLinks() {
+      return [
+        {
+          title: 'Home',
+          to: '/'
+        },
+        {
+          title: 'Flutter',
+          to: '/flutter'
+        }
+      ]
+    }
   },
 
   head: {
@@ -81,21 +151,6 @@ export default {
         content: 'Hey there! I\'m Mohammad Rahmani, a Flutter developer based in Kabul. I love Flutter, and I\'m always looking for new challenges. In this page you can find my 100 days of Flutter journey.',
       }
     ]
-  },
-
-  methods: {
-    breadcrumbLinks() {
-      return [
-        {
-          title: 'Home',
-          to: '/'
-        },
-        {
-          title: 'Flutter',
-          to: '/flutter'
-        }
-      ]
-    }
   }
 }
 </script>
