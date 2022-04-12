c335bf21439aee532e5a01fd13dce3e884b8e172
Mohammad Rahmani <rto1680@gmail.com>
2022-04-12 22:31:02 +0430

    Deploy

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
         <span class="font-light text-md">
           <a href="https://github.com/afgprogrammer" title="afgprogrammer - Github" class="underline text-gray-700" target="_blank"><span>Github</span></a>,&nbsp;
           <a href="https://youtube.com/afgprogrammer" title="afgprogrammer - Youtube Channel" class="underline text-gray-700" target="_blank"><span>Youtube</span></a>,&nbsp;
-          <a href="https://twitter.com/afgprogrammer" title="afgprogrammer - Twitter" class="underline text-gray-700" target="_blank"><span>Twitter</span></a>,&nbsp;
+          <a href="https://www.linkedin.com/in/afgprogrammer" title="My Resume - LinkedIn" class="underline text-gray-700" target="_blank"><span>LinkedIn (CV)</span></a>,&nbsp;
           <a href="https://instagram.com/afgprogrammer" title="afgprogrammer - Instagram Page" class="underline text-gray-700" target="_blank"><span>Instagram</span></a>
         </span>
         <p class="mt-6 text-sm text-gray-500">The website is build on NuxtJS. Thanks for visiting ❤️  & have a nice day</p>
