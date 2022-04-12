17fc53b011cff71ae451e641751e1588fee514ca
Mohammad Rahmani <rto1680@gmail.com>
2022-04-12 22:31:02 +0430

    Deploy

--- _nuxt/static/1649256913/php/foreach-in-php/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649256913",layout:"default",error:a,serverRendered:true,routePath:"\u002Fphp\u002Fforeach-in-php",config:{"google-adsense":{id:void 0,test:false},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"301a0b70"}}}}(null));
