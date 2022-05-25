8ea8df120db4ed9554bec7ff6dbf4c23c16f75fb
Mohammad Rahmani <rto1680@gmail.com>
2022-05-25 20:55:55 +0430

    Add heart

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,7 @@
           <a href="https://www.linkedin.com/in/afgprogrammer" title="My Resume - LinkedIn" class="underline text-gray-700" target="_blank"><span>LinkedIn (CV)</span></a>,&nbsp;
           <a href="https://instagram.com/afgprogrammer" title="afgprogrammer - Instagram Page" class="underline text-gray-700" target="_blank"><span>Instagram</span></a>
         </span>
-        <p class="mt-6 text-sm text-gray-500">The website is build on NuxtJS. Thanks for visiting  & have a nice day</p>
+        <p class="mt-6 text-sm text-gray-500">The website is build on NuxtJS. Thanks for visiting ❤️  & have a nice day</p>
     </footer>
 </template>
 
