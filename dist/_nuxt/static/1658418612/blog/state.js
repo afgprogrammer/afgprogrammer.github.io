e709e1ac844e300a22894a20db2ba5a39fd9ebb8
Mohammad Rahmani <rto1680@gmail.com>
2022-07-21 21:32:02 +0430

    Deploy

--- dist/_nuxt/static/1658418612/blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658418612",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{"google-adsense":{id:void 0,test:false},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f519be7"}}}}(null));
