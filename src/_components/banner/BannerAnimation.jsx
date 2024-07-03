9a4dfe562e0939d3506370666df98416f4dd8b23
Mohammad Rahmani <rto1680@gmail.com>
2024-07-03 13:31:43 +0330

    Add banner animation component

---
+++ src/_components/banner/BannerAnimation.jsx
@@ -0,0 +1,42 @@
+import classes from "@/_components/banner/BannerAnimation.module.css";
+
+const BannerAnimation = ({ images, speed = 5000 }) => {
+  return (
+    <div className={classes.inner}>
+      <div className={classes.wrapper}>
+        <section
+          className={classes.section}
+          style={{ "--speed": `${speed}ms` }}
+        >
+          {images.map(({ id, image }) => (
+            <div className={classes.image} key={id}>
+              <img src={image} alt={id} />
+            </div>
+          ))}
+        </section>
+        <section
+          className={classes.section}
+          style={{ "--speed": `${speed}ms` }}
+        >
+          {images.map(({ id, image }) => (
+            <div className={classes.image} key={id}>
+              <img src={image} alt={id} />
+            </div>
+          ))}
+        </section>
+        <section
+          className={classes.section}
+          style={{ "--speed": `${speed}ms` }}
+        >
+          {images.map(({ id, image }) => (
+            <div className={classes.image} key={id}>
+              <img src={image} alt={id} />
+            </div>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export { BannerAnimation };
